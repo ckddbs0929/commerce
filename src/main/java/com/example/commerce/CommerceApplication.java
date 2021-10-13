251ba7ffc199855ea1f99717f85d4bd03b7938ec
saemmilee <ckddbs0929@naver.com>
2021-10-13 16:57:39 +0900

    Initial commit

---
+++ src/main/java/com/example/commerce/CommerceApplication.java
@@ -0,0 +1,16 @@
+package com.example.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class CommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceApplication.class, args);
+
+		System.out.println("dgdgd");
+	}
+}
